78d4b9fe3c2e756424715adc1607399346f80b22
hhstore <hhstore@qq.com>
2018-08-29 19:49:05 +0800

    update(api) - client.

---
+++ crypto_exchange/api/rest.py
@@ -0,0 +1,24 @@
+
+
+class RestClient(object):
+
+    def get_order(self):
+        pass
+
+    def get_orders(self):
+        pass
+
+    def place_order(self):
+        pass
+
+    def place_orders(self):
+        pass
+
+    def cancel_order(self):
+        pass
+
+    def deposit(self):
+        pass
+
+    def withdraw(self):
+        pass
---
+++ crypto_exchange/api/websocket.py
@@ -0,0 +1,13 @@
+class WebSocketClient(object):
+
+    def auth(self):
+        pass
+
+    def k_line(self):
+        pass
+
+    def tickers(self):
+        pass
+
+    def order_books(self):
+        pass
