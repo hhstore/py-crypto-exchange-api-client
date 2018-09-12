cb3b7af77c7d1ac18d5482fec8fd498ba12bc2b1
dev <1946218941@qq.com>
2018-09-12 17:56:26 +0800

    okex spot ws client update.

--- crypto_exchange/exchanges/okex/okex_websocket/okex_ws_spot_client.py
+++ crypto_exchange/exchanges/okex/okex_websocket/okex_ws_spot_client.py
@@ -1,277 +1,287 @@
-# import websocket
-# import time
-# import sys
-# import json
-# import hashlib
-# import zlib
-# import base64
-#
-# api_key = ''
-# secret_key = ""
-#
-#
-#
-#
-# # spot trade
-# def spotTrade(channel, api_key, secretkey, symbol, tradeType, price='', amount=''):
-#     params = {
-#         'api_key': api_key,
-#         'symbol': symbol,
-#         'type': tradeType
-#     }
-#     if price:
-#         params['price'] = price
-#     if amount:
-#         params['amount'] = amount
-#     sign = buildMySign(params, secretkey)
-#     finalStr = "{'event':'addChannel','channel':'" + channel + "','parameters':{'api_key':'" + api_key + "',\
-#                 'sign':'" + sign + "','symbol':'" + symbol + "','type':'" + tradeType + "'"
-#     if price:
-#         finalStr += ",'price':'" + price + "'"
-#     if amount:
-#         finalStr += ",'amount':'" + amount + "'"
-#     finalStr += "},'binary':'true'}"
-#     return finalStr
-#
-#
-# # spot cancel order
-# def spotCancelOrder(channel, api_key, secretkey, symbol, orderId):
-#     params = {
-#         'api_key': api_key,
-#         'symbol': symbol,
-#         'order_id': orderId
-#     }
-#     sign = buildMySign(params, secretkey)
-#     return "{'event':'addChannel','channel':'" + channel + "','parameters':{'api_key':'" + api_key + "','sign':'" + sign + "','symbol':'" + symbol + "','order_id':'" + orderId + "'},'binary':'true'}"
-#
-#
-# # subscribe trades for self
-# def realtrades(channel, api_key, secretkey):
-#     params = {'api_key': api_key}
-#     sign = buildMySign(params, secretkey)
-#     return "{'event':'addChannel','channel':'" + channel + "','parameters':{'api_key':'" + api_key + "','sign':'" + sign + "'},'binary':'true'}"
-#
-#
-# # trade for future
-# def futureTrade(api_key, secretkey, symbol, contractType, price='', amount='', tradeType='', matchPrice='',
-#                 leverRate=''):
-#     params = {
-#         'api_key': api_key,
-#         'symbol': symbol,
-#         'contract_type': contractType,
-#         'amount': amount,
-#         'type': tradeType,
-#         'match_price': matchPrice,
-#         'lever_rate': leverRate
-#     }
-#     if price:
-#         params['price'] = price
-#     sign = buildMySign(params, secretkey)
-#     finalStr = "{'event':'addChannel','channel':'ok_futuresusd_trade','parameters':{'api_key':'" + api_key + "',\
-#                'sign':'" + sign + "','symbol':'" + symbol + "','contract_type':'" + contractType + "'"
-#     if price:
-#         finalStr += ",'price':'" + price + "'"
-#     finalStr += ",'amount':'" + amount + "','type':'" + tradeType + "','match_price':'" + matchPrice + "','lever_rate':'" + leverRate + "'},'binary':'true'}"
-#     return finalStr
-#
-#
-# # future trade cancel
-# def futureCancelOrder(api_key, secretkey, symbol, orderId, contractType):
-#     params = {
-#         'api_key': api_key,
-#         'symbol': symbol,
-#         'order_id': orderId,
-#         'contract_type': contractType
-#     }
-#     sign = buildMySign(params, secretkey)
-#     return "{'event':'addChannel','channel':'ok_futuresusd_cancel_order','parameters':{'api_key':'" + api_key + "',\
-#             'sign':'" + sign + "','symbol':'" + symbol + "','contract_type':'" + contractType + "','order_id':'" + orderId + "'},'binary':'true'}"
-#
-#
-# # subscribe future trades for self
-# def futureRealTrades(api_key, secretkey):
-#     params = {'api_key': api_key}
-#     sign = buildMySign(params, secretkey)
-#     return "{'event':'addChannel','channel':'ok_sub_futureusd_trades','parameters':{'api_key':'" + api_key + "','sign':'" + sign + "'},'binary':'true'}"
-#
-#
-# def on_open(self):
-#     # subscribe okcoin.com spot ticker
-#     self.send("{'event':'addChannel','channel':'ok_sub_spotusd_btc_ticker','binary':'true'}")
-#
-#     # subscribe okcoin.com future this_week ticker
-#     # self.send("{'event':'addChannel','channel':'ok_sub_futureusd_btc_ticker_this_week','binary':'true'}")
-#
-#     # subscribe okcoin.com future depth
-#     # self.send("{'event':'addChannel','channel':'ok_sub_futureusd_ltc_depth_next_week_20','binary':'true'}")
-#
-#     # subscrib real trades for self
-#     # realtradesMsg = realtrades('ok_sub_spotusd_trades',api_key,secret_key)
-#     # self.send(realtradesMsg)
-#
-#     # spot trade via websocket
-#     # spotTradeMsg = spotTrade('ok_spotusd_trade',api_key,secret_key,'ltc_usd','buy_market','1','')
-#     # self.send(spotTradeMsg)
-#
-#     # spot trade cancel
-#     # spotCancelOrderMsg = spotCancelOrder('ok_spotusd_cancel_order',api_key,secret_key,'btc_usd','125433027')
-#     # self.send(spotCancelOrderMsg)
-#
-#     # future trade
-#     # futureTradeMsg = futureTrade(api_key,secret_key,'btc_usd','this_week','','2','1','1','20')
-#     # self.send(futureTradeMsg)
-#
-#     # future trade cancel
-#     # futureCancelOrderMsg = futureCancelOrder(api_key,secret_key,'btc_usd','65464','this_week')
-#     # self.send(futureCancelOrderMsg)
-#
-#     # subscrbe future trades for self
-#     # futureRealTradesMsg = futureRealTrades(api_key,secret_key)
-#     # self.send(futureRealTradesMsg)
-#
-#
-# def on_message(self, evt):
-#     data = inflate(evt)  # data decompress
-#     print(data)
-#
-#
-# def inflate(data):
-#     decompress = zlib.decompressobj(
-#         -zlib.MAX_WBITS  # see above
-#     )
-#     inflated = decompress.decompress(data)
-#     inflated += decompress.flush()
-#     return inflated
-#
-#
-# def on_error(self, evt):
-#     print(evt)
-#
-#
-# def on_close(self, evt):
-#     print('DISCONNECT')
-#
-#
-# if __name__ == "__main__":
-#     websocket.enableTrace(False)
-#     if len(sys.argv) < 2:
-#         host = url
-#     else:
-#         host = sys.argv[1]
-#     ws = websocket.WebSocketApp(host,
-#                                 on_message=on_message,
-#                                 on_error=on_error,
-#                                 on_close=on_close)
-#     ws.on_open = on_open
-#     ws.run_forever()
+# -*- coding: utf-8 -*-
+import asyncio
+import hashlib
+import json
+import os
+import aiohttp
+import datetime
 
-import logging
-
-from pprint import pprint
-
-from crypto_exchange.exchanges.okex.okex_websocket.okex_ws import OKEx
-
-logger = logging.getLogger(__name__)
 API_KEY = "3b773537-bbae-4db9-9a9b-42069d7e1fbb"
 SECRET_KEY = "EFAABB4F616059E45557329A86D2B77C"
 
 
-def okex_spot_ticker(symbol: str):
-    """
-    订阅行情数据
-    :param symbol
-    :return:
-    """
-    okex_spot = OKEx()
-    okex_spot.send_sub("{'event':'addChannel','channel':'ok_sub_spot_%s_ticker'}" % symbol)
+class OkexSpotWebSocketClient(object):
 
-    yield from okex_spot.recv()
+    def __init__(self, api_key=None, secret_key=None):
+        self.url = "wss://real.okex.com:10441/websocket"
+        self.evt_ping = "{'event':'ping'}"
+        self.api_key = api_key or API_KEY
+        self.secret_key = secret_key or SECRET_KEY
+
+    # ping
+    @staticmethod
+    async def timer(ws, params):
+        """
+
+        :param ws:
+        :param params:
+        :param timeout:
+        :return:
+        """
+        # ping
+        await ws.send_str(params)
+        msg = await ws.receive()
+        print(f"pong msg: {msg}")
+        # sleep
+        # await asyncio.sleep(timeout)
+
+    # 消息订阅
+    async def subscribe(self, channel: str, keep_alive=True):
+        """
+        :param channel:
+        :param keep_alive:
+        :return:
+        """
+        session = aiohttp.ClientSession()
+        async with session.ws_connect(url=self.url) as ws:
+            if channel:
+                await ws.send_str(channel)
+            try:
+                await self.read_msg(ws=ws, keep_alive=keep_alive)
+            except aiohttp.ServerDisconnectedError as e:
+                if e.message == 'closed':
+                    # todo 重新连接
+                    pass
+
+    # read msg
+    async def read_msg(self, ws, keep_alive=True):
+        """
+        :param ws:
+        :param keep_alive:
+        :return:
+        """
+        start_timestamp = datetime.datetime.utcnow().timestamp()
+        async for msg in ws:
+            print('start:{}'.format(int(start_timestamp)))
+            if msg.type in (aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.ERROR):
+                print("websocket closed or msg type error")
+                # todo raise exception?
+                ws.close()
+                raise aiohttp.ServerDisconnectedError(message='closed')
+
+            print(f"ws msg:{msg}")
+            # todo 消息处理
+            data = json.loads(msg.data)
+            print(f"data: {data}")
+
+            if keep_alive:
+                intervel = datetime.datetime.utcnow().timestamp() - start_timestamp
+                if intervel >= 20:
+                    print(f"intervel: {intervel}")
+                    await self.timer(ws=ws, params=self.evt_ping)
+                    start_timestamp = datetime.datetime.utcnow().timestamp()
+
+    # 行情数据
+    async def ticker(self, symbol: str):
+        """
+        :param symbol: 交易对
+        :return:
+        """
+        event = "{'event':'addChannel','channel':'ok_sub_spot_%s_ticker'}" % symbol
+        print(f'event:{event}')
+
+        task = self.subscribe(channel=event)
+        return await task
+
+    # 币币市场深度(200增量数据返回)
+    async def spot_depth(self, symbol: str):
+        """
+        :param symbol: 交易对
+        :return: 第一次为全量数据
+        """
+        event = "{'event':'addChannel','channel':'ok_sub_spot_%s_depth'}" % symbol
+        print(f'event:{event}')
+
+        task = self.subscribe(channel=event)
+        return await task
+
+    # 市场深度
+    async def spot_depth_size(self, symbol: str, size: int):
+        """
+        :param symbol: 交易对
+        :param size: 深度条数
+        :return:
+            msg.data
+                [
+                    {
+                        "channel": "ok_sub_spot_bch_btc_depth_5",
+                        "data": {
+                            "asks": [], 买方深度
+                            "bids": [], 卖方深度
+                            "timestamp": 1504529432367, 服务器时间戳
+                        }
+                    }
+                ]
+        """
+        event = "{'event':'addChannel','channel':'ok_sub_spot_%s_depth_%s'}" % (symbol, size)
+        print(f'event:{event}')
+        task = self.subscribe(channel=event)
+        return await task
+
+    # 成交记录
+    async def spot_deals(self, symbol: str):
+        """
+        :param symbol: 交易对
+        :return:
+            msg.data
+                [{
+                    "channel":"ok_sub_spot_bch_btc_deals",
+                    "data":[["1001","2463.86","0.052","16:34:07","ask"]]
+                }]
+                data: 交易号,价格，成交量，时间，买卖类型
+        """
+        event = "{'event':'addChannel','channel':'ok_sub_spot_%s_deals'}" % symbol
+        print(f'event:{event}')
+        task = self.subscribe(channel=event)
+        return await task
+
+    # K线数据
+    async def kline(self, symbol: str, period: str):
+        """
+        :param symbol: 交易对
+        :param period: 周期
+        :return:
+            msg.data
+                [{
+                    "channel":"ok_sub_spot_bch_btc_kline_1min",
+                    "data":[
+                        ["1490337840000","995.37","996.75","995.36","996.75","9.112"]
+                    ]
+                }]
+                data: [时间，开盘价，最高价，最低价，收盘价，成交量]
+        """
+        periods = ["1min", "3min", "5min", "15min", "30min", "1hour",
+                   "2hour", "4hour", "6hour", "12hour", "day", "3day", "week"]
+
+        event = "{'event':'addChannel','channel':'ok_sub_spot_%s_kline_%s'}" % \
+                (symbol, period if period in periods else "1min")
+        print(f'event:{event}')
+
+        task = self.subscribe(channel=event)
+        return await task
+
+    @staticmethod
+    def sign(params: dict, secret_key):
+        """
+        :param params:
+        :param secret_key:
+        :return:
+        """
+        sign = ''
+        # 对参数进行排序,拼接数据
+        for key in sorted(params.keys()):
+            sign += key + "=" + str(params[key]) + "&"
+        data = sign + "secret_key=" + secret_key
+        # 签名
+        return hashlib.md5(data.encode("utf8")).hexdigest().upper()
+
+    # API用户登录
+    def login(self):
+        sign = self.sign(params={'api_key': self.api_key}, secret_key=self.secret_key)
+
+        params = {"event": "login",
+                  "parameters": {"api_key": self.api_key, "sign": sign}}
+        return params
+
+    # 用户交易订单
+    async def spot_order(self, symbol: str):
+        """
+        :param symbol: 交易对
+        :return:
+        """
+        session = aiohttp.ClientSession()
+        async with session.ws_connect(url=self.url) as ws:
+            # 登录
+            await ws.send_json(data=self.login())
+            msg = await ws.receive()
+            result = json.loads(msg.data)
+            print(f"login result: {result}")
+            if not result[0].get('data').get('result'):
+                # 登录失败
+                # todo 登录失败处理
+                pass
+            event = {
+                "event": "addChannel",
+                "channel": "ok_sub_spot_%s_balance" % symbol,
+                "parameters": {
+                    "api_key": self.api_key,
+                    "symbol": symbol,
+                    "sign": self.sign(params={"symbol": symbol}, secret_key=self.secret_key)
+                }
+            }
+            # event = "{'event':'addChannel','channel':'ok_sub_spot_%s_order'}" % symbol
+            await ws.send_json(event)
+            await self.timer(ws, self.evt_ping)
+            try:
+                await self.read_msg(ws=ws, keep_alive=True)
+            except aiohttp.ServerDisconnectedError as e:
+                if e.message == 'closed':
+                    # todo 重新连接
+                    pass
+
+    # 用户账户信息
+    async def spot_balance(self, symbol: str):
+        """
+        :param symbol: 交易对
+        :return:
+        """
+        session = aiohttp.ClientSession()
+        async with session.ws_connect(url=self.url) as ws:
+            # 登录
+            await ws.send_json(data=self.login())
+            msg = await ws.receive()
+            result = json.loads(msg.data)
+            print(f"login result: {result}")
+            if not result[0].get('data').get('result'):
+                # 登录失败
+                # todo 登录失败处理
+                pass
+            # event = "{'event':'addChannel','channel':'ok_sub_spot_%s_balance'}" % symbol
+            event = {
+                "event": "addChannel",
+                "channel": "ok_sub_spot_%s_balance" % symbol,
+                "parameters": {
+                    "api_key": self.api_key,
+                    "symbol": symbol,
+                    "sign": self.sign(params={"symbol": symbol}, secret_key=self.secret_key)
+                }
+            }
+            await ws.send_json(event)
+            try:
+                await self.read_msg(ws=ws, keep_alive=True)
+            except aiohttp.ServerDisconnectedError as e:
+                if e.message == 'closed':
+                    # todo 重新连接
+                    pass
 
 
-def okex_spot_depth(symbol: str):
-    """
-    订阅币币市场深度(200增量数据返回)
-    第一次返回全量数据，根据接下来数据对第一次返回数据进行如下操作：删除（量为0时）；修改（价格相同量不同）；增加（价格不存在）
-    :param symbol:
-    :return:
-    """
-    okex_spot = OKEx()
-    okex_spot.send_sub("{'event':'addChannel','channel':'ok_sub_spot_%s_depth'}" % symbol)
+if __name__ == '__main__':
+    print('Type "exit" to quit')
+    loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    # loop.run_until_complete(exchange())
 
-    yield from okex_spot.recv()
+    # url = "wss://real.okex.com:10441/websocket"
+    # event = "{'event':'addChannel','channel':'ok_sub_spot_X_ticker'}"
+    # event = "{'event':'addChannel','channel':'ok_sub_spot_bch_btc_ticker'}"
+    # ex = okex_ws(url=url, params=event)
 
-
-def okex_spot_depth_size(symbol: str, size: str):
-    """
-    订阅市场深度
-    :param symbol:值为币对，如ltc_btc
-    :param size值为获取深度条数，如5，10，20
-    :return:
-    """
-    okex_spot = OKEx()
-    okex_spot.send_sub("{'event':'addChannel','channel':'ok_sub_spot_%s_depth_%s'}" % (symbol, size))
-
-    yield from okex_spot.recv()
-
-
-def okex_spot_deals(symbol: str):
-    """
-    订阅成交记录
-    :param symbol:值为币对，如ltc_btc
-    :return:
-    """
-    okex_spot = OKEx()
-    okex_spot.send_sub("{'event':'addChannel','channel':'ok_sub_spot_%s_deals'}" % symbol)
-
-    yield from okex_spot.recv()
-
-
-def okex_spot_k_line_size(symbol: str, size: str):
-    """
-     订阅K线数据
-    :param symbol:值为币对，如ltc_btc
-    :param size:值为K线时间周期，如1min, 3min, 5min, 15min, 30min, 1hour, 2hour, 4hour, 6hour, 12hour, day, 3day, week
-    :return:
-    """
-    okex_spot = OKEx()
-    okex_spot.send_sub("{'event':'addChannel','channel':'ok_sub_spot_%s_kline_%s'}" % (symbol, size))
-
-    yield from okex_spot.recv()
-
-
-def okex_spot_login():
-    """
-    # TODO 文档描述不清晰
-    login 登录事件(个人信息推送)
-    api_key	用户申请的APIKEY
-    sign	请求参数的签名
-    :return:
-    """
-
-    params = {}
-    api_key = API_KEY
-    secret_key = SECRET_KEY
-
-    okex_spot = OKEx()
-    sign = okex_spot.sign(params, secret_key)
-    okex_spot.send_sub("""{"event":"login","parameters":{"api_key":%s,"sign":%s}}""" % (api_key, sign))
-
-    yield from okex_spot.recv()
-
-
-# 订阅行情数据
-# data = okex_spot_ticker('bch_btc')
-
-# 订阅币币市场深度(200增量数据返回)
-# data = okex_spot_depth('bch_btc')
-
-# 订阅市场深度
-# data = okex_spot_depth_size('bch_btc', '10')
-
-# 订阅成交记录
-data = okex_spot_deals('bch_btc')
-
-# login 登录事件(个人信息推送)
-# data = okex_spot_login()
-for i in data:
-    pprint(i)
+    c = OkexSpotWebSocketClient()
+    # task = c.ticker(symbol="bch_btc")
+    # task = c.spot_depth(symbol="bch_btc")
+    # task = c.spot_depth_size(symbol="bch_btc", size=5)
+    # task = c.spot_deals(symbol="bch_btc")
+    # task = c.kline(symbol="bch_btc", period="1min")
+    task = c.spot_order(symbol="bch_btc")
+    # task = c.spot_balance(symbol="bch_btc")
+    loop.run_until_complete(task)
