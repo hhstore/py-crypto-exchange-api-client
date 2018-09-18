1d77022a0ef6220b6454747d90f0ea444447397f
dev <1946218941@qq.com>
2018-09-18 18:10:03 +0800

    constant update.

---
+++ crypto_exchange/constants.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class KlineCycle:
+
+    KLine1Min = 1
+    KLine3Min = 2
+    KLine5Min = 3
+    KLine15Min = 4
+    KLine30Min = 5
+    KLine1hour = 6
+    KLine4hour = 7
+    KLineDay = 8
+    KLine5Day = 9
+    KLineWeek = 10
+    KLineMonth = 11
+    KLineYear = 12
