4ebb80e68fbb0b630033af2fd3efa0df475c4889
hhstore <hhstore@qq.com>
2018-09-19 18:16:12 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     include_package_data=True,
     keywords='crypto_exchange',
     name='crypto_exchange',
-    packages=find_packages(include=['crypto_exchange']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
