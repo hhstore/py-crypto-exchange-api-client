15a871d0a464d6afbefd85b7049e12d9c1ea3909
dev <1946218941@qq.com>
2018-09-19 15:14:44 +0800

    kafka client update.

--- crypto_exchange/utils/kafka_util.py
+++ crypto_exchange/utils/kafka_util.py
@@ -4,12 +4,24 @@ from kafka import KafkaConsumer
 import json
 
 
-producer = KafkaProducer(bootstrap_servers='192.168.50.190:9092',
-                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+class JsonKafkaClient:
 
-consumer = KafkaConsumer('test-python',
-                         group_id='python',
-                         bootstrap_servers='192.168.50.190:9092',
-                         value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+    @staticmethod
+    def producer(host: str):
+        producer = KafkaProducer(bootstrap_servers=host or '192.168.50.190:9092',
+                                 value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+        return producer
 
-
+    @staticmethod
+    def consumer(topics, host: str, group_id):
+        if isinstance(topics, list) or isinstance(topics, str):
+            consumer = KafkaConsumer(topics,
+                                     group_id=group_id or 'default',
+                                     bootstrap_servers=host or '192.168.50.190:9092',
+                                     value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+            return consumer
+        else:
+            return KafkaConsumer('default',
+                                 group_id=group_id or 'default',
+                                 bootstrap_servers=host or '192.168.50.190:9092',
+                                 value_deserializer=lambda m: json.loads(m.decode('utf-8')))
